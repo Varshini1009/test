a686e627c264c422871c924bc1a1ec1520bfb6e5
Selvarasan <ragavarshini.selvarasan@stryker.com>
2024-09-05 17:10:51 +0530

    var Code

--- var.py
+++ var.py
@@ -1,1 +1,11 @@
-print("This is Ragavarshini")
+# Function to add two numbers
+def add_two_numbers(num1, num2):
+    return num1 + num2
+
+# Example usage
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+result = add_two_numbers(number1, number2)
+print("The sum of", number1, "and", number2, "is", result)
+
